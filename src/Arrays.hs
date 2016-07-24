85b456b026fa05447b31ccf34464738e3fe84c25
Mouna Cheikhna <cheikhnamouna@gmail.com>
2016-07-24 22:40:56 +0200

    differentiate with type fns that alter the state and those that dont and create an interpreter with this functionality.

--- src/Arrays.hs
+++ src/Arrays.hs
@@ -76,3 +76,37 @@ exec' (If t c d) = eval' t >>= \a -> if a == 0 then exec' c else exec' d
 elab' :: Prog -> Int
 elab' (Prog c t) = block 0 (exec' c >>= \() -> eval' t >>= \a -> return a)
 
+-- Array readers
+type M' a = State -> a
+
+return' :: a -> M' a
+return' a = \x -> a
+
+(>>>=) :: M' a -> (a -> M' b) -> M' b -- >>>= same as >>= just for M'
+m >>>= k = \x -> let a = m x in k a x
+
+fetch' :: Ix -> M' Val
+fetch' i = \x -> index i x
+
+
+-- since computations that read state (M') are subset of thos that read & write it (M)
+-- let's coerce M' monad into M monad
+coerce :: M' a -> M a
+coerce m = \x -> let a = m x in (a, x)
+
+-- rewrite our interpreter with array readers (uses M' for computation that only read and M for those that read & write like assign)
+eval'' :: Term -> M' Int
+eval'' (Var i) = fetch' i
+eval'' (Con a) = return' a
+eval'' (Add t u) = eval'' t >>>= \a -> eval'' u >>>= \b -> return' (a + b)
+
+exec'' :: Comm -> M ()
+exec'' (Asgn i t) = coerce (eval'' t) >>= \a -> assign i a
+exec'' (Seq c d) = exec'' c >>= \() -> exec'' d >>= \() -> return ()
+exec'' (If t c d) = coerce (eval'' t) >>= \a -> if a == 0 then exec'' c else exec'' d
+
+elab'' :: Prog -> Int
+elab'' (Prog c t) = block 0 (exec'' c >>= \() -> coerce (eval'' t) >>= \a -> return a)
+
+
+
